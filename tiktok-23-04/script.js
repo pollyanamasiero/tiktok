c2873f1bb1467bc9cbb1c754291e7c3ecdb4c9f1
pollyanamasiero <pollyanamasiero@hotmail.com>
2025-04-24 14:57:00 -0300

    Primeiro commit

---
+++ tiktok-23-04/script.js
@@ -0,0 +1,5 @@
+let cliques = 0;
+            function contador() {
+                cliques++;
+                document.getElementById("resultado").textContent = "Cliques: " + cliques;
+            }
