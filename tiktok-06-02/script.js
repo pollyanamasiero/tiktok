c9dba87021657946ac723c6458613795562aa1fb
pollyanamasiero <pollyanamasiero@hotmail.com>
2025-06-03 23:03:38 -0300

    formatando os arquivos

--- tiktok-06-02/script.js
+++ tiktok-06-02/script.js
@@ -3,19 +3,19 @@ const button = document.getElementById('verificarBtn');
 const resultado = document.getElementById('resultado');
 
 button.addEventListener('click', () => {
-  const numero = parseInt(input.value);
+    const numero = parseInt(input.value);
 
-  if (isNaN(numero)) {
-    resultado.textContent = 'Por favor, digite um número válido.';
-    resultado.style.color = 'red';
-    return;
-  }
+    if (isNaN(numero)) {
+        resultado.textContent = 'Por favor, digite um número válido.';
+        resultado.style.color = 'red';
+        return;
+    }
 
-  if (numero % 2 === 0) {
-    resultado.textContent = `O número ${numero} é PAR.`;
-    resultado.style.color = 'green';
-  } else {
-    resultado.textContent = `O número ${numero} é ÍMPAR.`;
-    resultado.style.color = 'purple';
-  }
+    if (numero % 2 === 0) {
+        resultado.textContent = `O número ${numero} é PAR.`;
+        resultado.style.color = 'green';
+    } else {
+        resultado.textContent = `O número ${numero} é ÍMPAR.`;
+        resultado.style.color = 'purple';
+    }
 });
